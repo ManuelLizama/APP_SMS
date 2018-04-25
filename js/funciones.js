542d5cf6fde4a12189129ebf593e01169650bd21
manuellizama <manuellizamaac@outlook.cl>
2018-04-25 00:28:54 -0300

    APP SMS

--- js/funciones.js
+++ js/funciones.js
@@ -1,3 +1,27 @@
+var lat = '-33.4349083';
+var lon = '-70.6170132';
+document.addEventListener("deviceready", onDeviceReady, false);
+
+
+function showAlert(msj)
+{
+    navigator.notification.alert(
+        msj,  // message
+        'UNAB',   // title
+        ''    // buttonName
+    );
+}//fin function mensaje.
+
+// PhoneGap is ready
+function onDeviceReady() 
+{
+// Do cool things here...
+  //getPosition();
+  if (! SMS ) { alert( 'SMS plugin not ready' ); return; }
+  
+}
+ 
+
 function soloLetras(e)
 {
    key = e.keyCode || e.which;
@@ -7,122 +31,78 @@ function soloLetras(e)
 
    tecla_especial = false;
 
-	if(letras.indexOf(tecla)==-1 && !tecla_especial)
-	{
-		return false;
-	}
+  if(letras.indexOf(tecla)==-1 && !tecla_especial)
+  {
+    return false;
+  }
 }
 //function soloNumeros(evt)
 function soloNumeros(e)
 {
-	tecla = (document.all) ? e.keyCode : e.which; 
-	if (tecla==8) return true; 
-	patron =/^[0-9.]+$/;//este acepta punto(.), si se quiere eliminar borrar el punto despues del 9. 
-	te = String.fromCharCode(tecla); 
-	return patron.test(te);
+  tecla = (document.all) ? e.keyCode : e.which; 
+  if (tecla==8) return true; 
+  patron =/^[0-9.]+$/;//este acepta punto(.), si se quiere eliminar borrar el punto despues del 9. 
+  te = String.fromCharCode(tecla); 
+  return patron.test(te);
 }
 
 function validaralpha(e) 
 { 
-	tecla = (document.all) ? e.keyCode : e.which; 
-	if (tecla==8) return true; 
-	patron =/[\ w\w.&ñ]/;//este acepta espacios entre medio, si se quiere eliminar los espacios poner [\w] 
-	te = String.fromCharCode(tecla); 
-	return patron.test(te);
+  tecla = (document.all) ? e.keyCode : e.which; 
+  if (tecla==8) return true; 
+  patron =/[\ w\w.&ñ]/;//este acepta espacios entre medio, si se quiere eliminar los espacios poner [\w] 
+  te = String.fromCharCode(tecla); 
+  return patron.test(te);
 }
 
-function operacion()
-{
-	var select=$("#operacion option:selected").val();//text() para texto de select option
-	var nro1=document.getElementById('nro1').value;
-	var nro2=document.getElementById('nro2').value;
-	if(nro1=='' || nro2=='')
-	{
-		alert('Debe Ingresar los valores!');
-	}
-	else
-	{
-	//alert(select);
-	if(select==0)
-	{
-		alert('Debe seleccionar Operación!!!');
-	}
-	else if(select==1)
-	{
-		funcion='suma';
-	}
-	else if(select==2)
-	{
-		funcion='resta';
-	}
-	else if(select==3)
-	{
-		funcion='mult';
-	}
-	else if(select==4)
-	{
-		funcion='division';
-	}
-	if(select > 0)
-	{		
-		//alert(funcion);
-		$.ajax({
-		cache: false,
-		// puede ser GET, POST
-		type: "POST",  							
-		// Tipo de retorno
-		dataType: "html",
-		// pagina php que recibe la llamada
-		url: "http://72.14.183.67/ws/",  							
-		// datos, ej: $_POST['data']
-		data: {
-			funcion:funcion,
-			n1:nro1,
-			n2:nro2				
-		},
-		beforeSend: function(){  
-                    document.getElementById('divCargando').style.display="block";
-                    $("#labelCargando").html('Cargando...');	
-		},
-		// acciones cuando me retorna algo el PHP
-		success: function( msg){
-			//console.log(msg);
-                        document.getElementById('divCargando').style.display="none";
-			if(msg=='error')
-			{
-				alert('Ha ocurrido un Error.');
-			}
-			else if(msg=='cero')
-			{
-				alert('Error!, Nro2 no puede ser cero.');
-                                reiniciar();
-			}
-			else if(msg=='NO ES UN NRO!')
-			{
-				alert('Error! lo ingresado no es un numero.');
-			}
-			else
-			{
-				document.getElementById('divResultado').style.display="block";
-				$("#labelResultado").html('Resultado de la '+$("#operacion option:selected").text()+'='+msg);
-				
-			}
-		},							
-		// acciones cuando hay error en comunicacion el el php
-		error: function(xhr, status,msg2 ){
-			//alert('4');			
-			console.log(xhr);
-		}
-	});//fin ajax
-	}//fin else
-	}
+function verUbicacion(){
+        if(lat == 0)
+        {
+            alert('Primero enciende el GPS!');
+        }
+        else{
+        	link="https://www.google.com/maps?q="+lat+","+lon;
+            var ref = window.open(link, '_blank', 'location=yes');
+         	ref.addEventListener('loadstart', function(event) { showAlert('Url: ' + event.url); });
+           
+        }
 }
+
+
+function sendSMS()
+{
+  var fono=document.getElementById('fono').value;
+  var msg = document.getElementById('msg').value;
+  if(fono=='' || msg =='')
+  {
+	  if(fono==''){
+      alert('Debe Ingresar el numero!');
+    }
+    else if(msg==''){
+      alert('Debe Ingresar el mensaje!');
+    }
+  }
+  else
+  {
+  	// lat=position.coords.latitude;
+  	// lon=position.coords.longitude;
+    var textoURl = "y no tengo GPS Activado,";
+	  if(lat != 0){
+		  textoURl = "https://www.google.com/maps?q="+lat+","+lon;
+	  }
+	  
+	 if(SMS) 
+	 {
+	   SMS.sendSMS(fono, msg + "Ubicacion: "+ textoURl, function () { alert('Message sent successfully');}, function (e) { alert('Message Failed:' + e);});
+	 }
+	  
+  }
+}
+   
+
 
 function reiniciar()
 {
-	document.getElementById('nro1').value='';
-	document.getElementById('nro2').value='';
-	$("#operacion").val(0);
-	document.getElementById('divResultado').style.display="none";
-        document.getElementById('divCargando').style.display="none";
+	document.getElementById('fono').value='';
+	document.getElementById('msg').value='';
 }
